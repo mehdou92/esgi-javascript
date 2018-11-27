1a004daba781cfa49a8c219a4e3554fa7284cfa5
Mehdi ABKHOUKH <mehdiab92@gmail.com>
2018-11-27 15:33:10 +0100

    5

--- exercice-2/type-check.js
+++ exercice-2/type-check.js
@@ -1,31 +1,47 @@
 function type_check_v1(elem, type) {
 
     switch (type) {
-            case 'boolean':
-                return typeof elem === "boolean";
+        case 'boolean':
+            return typeof elem === "boolean";
 
-            case 'null' :
-                return elem === null;
+        case 'null' :
+            return elem === null;
 
-            case 'undefined':
-                return elem === undefined;
+        case 'undefined':
+            return elem === undefined;
 
-            case 'number':
-                return typeof elem === "number";
+        case 'number':
+            return typeof elem === "number";
 
-            case 'string':
-                return typeof elem === "string";
+        case 'string':
+            return typeof elem === "string";
 
-            case 'object' :
-                return typeof elem === "object";
+        case 'array':
+            return !!Array.isArray(elem);
 
-            default:
-                return false;
+        case 'object' :
+            if (typeof elem === "object" && Array.isArray(elem))
+                return true;
+            else return typeof elem === "object";
     }
 }
 
-//console.log(type_check_v1(undefined, 'undefined'));
-//console.log(type_check_v1('toto', 'undefined'));
-//type_check_v1('string', 'string');
-//type_check_v1(1, 'number');
-//type_check_v1(null, 'number');
+function myUndefined() {
+    return undefined;
+}
+
+function myFunction() {
+    console.log('beoeo');
+}
+
+console.log(type_check_v1(1, "number"));
+console.log(type_check_v1("string", "number"));
+console.log(type_check_v1(true, "number"));
+console.log(type_check_v1({"prop1": 1}, "object"));
+console.log(type_check_v1(null, "null"));
+console.log(type_check_v1(null, "object"));
+console.log(type_check_v1(myUndefined, "undefined"));
+console.log(type_check_v1("string", "string"));
+console.log(type_check_v1([1, 2, 3], "array"));
+console.log(type_check_v1(myFunction, "function"));
+console.log(type_check_v1([1, 2, 3], "object"));
