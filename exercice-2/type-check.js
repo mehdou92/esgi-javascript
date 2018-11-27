5dacc297fc242e157822df4c11e8252f4b026ed1
Mehdi ABKHOUKH <mehdiab92@gmail.com>
2018-11-27 15:07:38 +0100

    1

---
+++ exercice-2/type-check.js
@@ -0,0 +1,32 @@
+function type_check_v1(elem, type) {
+
+    switch (type) {
+            case 'boolean':
+                return typeof elem === "boolean";
+
+            case 'null' :
+                return elem === null;
+
+            case 'undefined':
+                return elem === undefined;
+
+            case 'number':
+                return typeof elem === "number";
+
+            case 'string':
+                return typeof elem === "string";
+
+            case 'object' :
+                return typeof elem === "object";
+
+            default:
+                console.log('error type doesn\'t exist');
+                break;
+    }
+}
+
+console.log(type_check_v1(undefined, 'undefined'));
+console.log(type_check_v1('toto', 'undefined'));
+//type_check_v1('string', 'string');
+//type_check_v1(1, 'number');
+//type_check_v1(null, 'number');
