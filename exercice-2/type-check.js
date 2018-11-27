4420d6c7da99e3a263f2b222f8e8d4fa94bdd148
Mehdi ABKHOUKH <mehdiab92@gmail.com>
2018-11-27 16:05:02 +0100

    8

--- exercice-2/type-check.js
+++ exercice-2/type-check.js
@@ -21,7 +21,9 @@ function type_check_v1(elem, type) {
 
         case 'object' :
             if (typeof elem === "object" && Array.isArray(elem))
-                return true;
+                return false;
+            else if(typeof elem === "object" && elem === null)
+                return false;
             else return typeof elem === "object";
     }
 }
@@ -34,14 +36,14 @@ function myFunction() {
     console.log('beoeo');
 }
 
-console.log(type_check_v1(1, "number"));
-console.log(type_check_v1("string", "number"));
-console.log(type_check_v1(true, "number"));
-console.log(type_check_v1({"prop1": 1}, "object"));
-console.log(type_check_v1(null, "null"));
-console.log(type_check_v1(null, "object"));
-console.log(type_check_v1(myUndefined, "undefined"));
-console.log(type_check_v1("string", "string"));
-console.log(type_check_v1([1, 2, 3], "array"));
-console.log(type_check_v1(myFunction, "function"));
-console.log(type_check_v1([1, 2, 3], "object"));
+//console.log(type_check_v1(1, "number"));
+//console.log(type_check_v1("string", "number"));
+//console.log(type_check_v1(true, "number"));
+//console.log(type_check_v1({"prop1": 1}, "object"));
+//console.log(type_check_v1(null, "null"));
+//console.log(type_check_v1(null, "object"));
+//console.log(type_check_v1(myUndefined, "undefined"));
+//console.log(type_check_v1("string", "string"));
+//console.log(type_check_v1([1, 2, 3], "array"));
+//console.log(type_check_v1(myFunction, "function"));
+//console.log(type_check_v1([1, 2, 3], "object"));
